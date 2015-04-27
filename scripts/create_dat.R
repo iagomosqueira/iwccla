f4005bfabe412ee2c778ce344094ebe62364b843
kellijohnson <kellifayejohnson@gmail.com>
2015-04-27 06:36:45 -0700

    Fixed bug in create_dat. Format bias and remove space ifreq.

--- scripts/create_dat.R
+++ scripts/create_dat.R
@@ -27,7 +27,37 @@
 #'   catch.
 #' @param nprot A numeric value specifying the number of years of pre-management
 #'   protection.
-#'
+#' @param msyl A numeric value specifying the true maximum sustainable yield level.
+#'   Where level refers to the terminal population divided by carrying capacity.
+#' @param msyr1 A numeric value specifying the true maximum sustainable yield rate.
+#'   Where rate refers to the fishing rate to obtain the maximum sustainable yield.
+#' @param depl A numeric value specyifying the initial depletion level.
+#' @param k99 A numeric value specifying how carying capacity changes with time.
+#'   More details are needed here. \link{\code{k99yr}}.
+#' @param msyr99 A numeric value specifying how the maximum sustainable yield rate
+#'   changes with time. More details are needed here. \link{\code{msyr99yr}}.
+#' @param istep A numeric value specifying the chosen option for time-varying
+#'   maximum sustainable yield rate. More details are needed here.
+#' @param mat1 A numeric value specifying maturity 1. More details are needed here.
+#' @param erate A numeric value specifying the probability that the population will
+#'   undergo an epidemic in any given year.
+#' @param component A numeric value of \code{0:2} specifying which component
+#'   density-dependence acts on. Where \code{0} is the exploitable, \code{1} is the
+#'   1+ population, and \code{2} is the mature population.
+#' @param ifreq A numeric value specifying the frequency at which surveys
+#'   are conducted.
+#' @param bias0 A numeric value specifying the bias in abundance estimates.
+#' @param cv1est A numeric value specifying the coefficient of variation for the
+#'   coefficient of variation estimates.
+#' @param inita A numeric value specifying an intial value for the parameter
+#'   specifying the resilience parameter *A*.
+#' @param initz A numeric value specifying an intial value for the parameter
+#'   specifying the degree of compensation *z*.
+#' @param k99yr A numeric value specyifying the year in which carrying capacity
+#'   changes to linearly, as specified by \code{k99} * *K*.
+#' @param msyr99yr A numeric value specyifying the year in which maximum
+#'   sustainable yield rate changes to linearly, as specified by
+#'   \code{msyr99} * *MSYR*.
 #' @return Invisibly returns lines necessary to produce a data file for the IWC
 #'   CLA MANTST program.
 ##' @seealso \code{\link{functionname}}
@@ -36,9 +66,9 @@
 
 create_dat <- function(out, case = "T1A-D1",
   comment = "Written by create_dat by (KFJ)", optran = 0, optb = 0, optc = 0,
-  optdt = 0, optdet = 0, optsur = 0, ntrial = 400, nyear = 100, npcat = 30,
+  optdt = 0, optdet = 0, optsur = 0, ntrial = 400, nyear = 300, npcat = 30,
   nprot = 0, msyl = 0.6, msyr1 = 0.01, depl = 0.99, k99 = 0.000, msyr99 = 0.000,
-  istep = 0, mat1 = 7.00, erate = 0.00, component = 1, ifreq = 5, bias0 = 0,
+  istep = 0, mat1 = 7.00, erate = 0.00, component = 1, ifreq = 5, bias0 = 1.0,
   cv1est = 0.20, inita = 0.20, initz = 0.20, k99yr = NULL, msyr99yr = NULL) {
 
   # Set up which component density dependence acts on
@@ -63,7 +93,7 @@ create_dat <- function(out, case = "T1A-D1",
   dat[10] <- paste0("STOCHASTICITY OPTION                 OPTDET    ", optdet)
   if (!optsur %in% 0:1) stop(paste("optsur (0:1) is", optdet))
   dat[11] <- paste0("SURVEY COSTS OPTION                  OPTSUR    ", optsur)
-  dat[12] <- paste0("No. OF TRIALS                        NTRIAL    ", ntrial)
+  dat[12] <- paste0("No. OF TRIALS                        NTRIAL  ", ntrial)
   dat[13] <- paste0("No. OF YEARS IN SIMULATION           NYEAR  ", nyear)
   dat[14] <- paste0("No. OF YEARS OF PREMANAGEMENT CATCH  NPCAT    ", npcat)
   dat[15] <- paste0("YEARS OF PREMANAGEMENT PROTECTION    NPPROT    ", nprot)
@@ -88,10 +118,11 @@ create_dat <- function(out, case = "T1A-D1",
   dat[34] <- paste0("MSYR COMPONENT                       OPTF     ", optf)
   dat[35] <- paste0("MSYL COMPONENT                       OPTMSYL  ", optmsyl)
   dat[36] <- paste0("DENSITY-DEPENDENT COMPONENT          OPTDD    ", optdd)
-  dat[37] <- paste0("FREQUENCY OF ABUNDANCE ESTIMATES     IFREQ     ", ifreq)
-  dat[38] <- paste0("YEAR OF LAST SURVEY                  ENDSUR ", nyear + 1)
-  dat[39] <- paste0("YEAR CV CHANGES                      IYRCV  ", nyear + 1)
-  dat[40] <- paste0("BIAS IN ABUNDANCE ESTIMATES          BIAS0    ", bias0)
+  dat[37] <- paste0("FREQUENCY OF ABUNDANCE ESTIMATES     IFREQ    ", ifreq)
+  dat[38] <- paste0("YEAR OF LAST SURVEY                  ENDSUR ", nyear)
+  dat[39] <- paste0("YEAR CV CHANGES                      IYRCV  ", nyear)
+  dat[40] <- paste0("BIAS IN ABUNDANCE ESTIMATES          BIAS0    ",
+    as.character(format(bias0, digits = 2, nsmall = 2)))
   dat[41] <- paste0("CV OF CV ESTIMATES (1st)             CV1EST   ", cv1est)
   dat[42] <- paste0("PROCESS ERROR PARAMETER              ETA      1.00")
   dat[43] <- paste0("MINIMUM No. OF DEGREES OF FREEDOM    DOFMIN   5.00")
