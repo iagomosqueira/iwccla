6dbc6fa7129c27481e26a8b9ed01549da8c50270
kellijohnson <kellifayejohnson@gmail.com>
2015-05-13 20:45:33 -0700

    Remove calls in plot code

--- R/plot_curve.R
+++ R/plot_curve.R
@@ -1,7 +1,17 @@
-
+#' Create a response curve for the *Catch Limit Algorithm*
+#'
+#' @details
+#'
+#' @param orig A \code{data.frame} of \code{RESOUT.RRR}
+#' @param alt A \code{data.frame} of \code{RESOUT.RRR}
+#' @param keep A \code{data.frame} of trials with specific names and attributes.
+#' @param out A character value to save the plot to, no extension necessary.
+#' @return Nothing is returned to the console, instead a plot is saved to the disk.
+##' @seealso \code{\link{functionname}}
+#' @author Kelli Faye Johnson
+#' @export
 
 plot_curve <- function(orig, alt, keep, out) {
-  on.exit(dev.off())
   num <- dim(keep)[1]
   symb <- LETTERS[1:num]
   xlim <- c(-1, num + 2)
@@ -25,239 +35,49 @@ plot_curve <- function(orig, alt, keep, out) {
     tck = 0.05, mgp = c(3, 0.1, 0))
   errbar(x = 1:dim(orig)[1], y = orig[, 2], yplus = orig[, 4], frame.plot = FALSE,
     yminus = orig[, 3], xaxt = "n", ylim = limtc, pch = symb, xlim = xlim)
+  axis(2, at = seq(limtc[1], limtc[2], by = .25)[seq(2,11,by = 2)], label = FALSE)
   mtext(side = 3, "Orig", line = -2, cex = little)
   errbar(x = 1:dim(alt)[1], y = alt[, 2], yplus = alt[, 4], frame.plot = FALSE,
     yminus = alt[, 3], xaxt = "n", ylim = limtc, pch = symb, xlim = xlim, yaxt = "n")
   axis(2, labels = FALSE)
+  axis(2, at = seq(limtc[1], limtc[2], by = .25)[seq(2,11,by = 2)], label = FALSE)
   mtext(side = 3, "Alt", line = -2, cex = little)
   text(x = xlim[1], y = limtc[2] * 1.05, "Total catch", cex = big, xpd = NA)
 
   errbar(x = 1:dim(orig)[1], y = orig[, 6], yplus = orig[, 8], frame.plot = FALSE,
     yminus = orig[, 7], xaxt = "n", ylim = limpf, pch = symb, xlim = xlim)
     mtext(side = 3, "Orig", line = -2, cex = little)
+  axis(2, at = seq(limpf[1], limpf[2], by = 0.1)[seq(2,11,by = 2)], label = FALSE)
   errbar(x = 1:dim(alt)[1], y = alt[, 6], yplus = alt[, 8], frame.plot = FALSE,
     yminus = alt[, 7], xaxt = "n", ylim = limpf, pch = symb, xlim = xlim, yaxt = "n")
   axis(2, labels = FALSE)
+  axis(2, at = seq(limpf[1], limpf[2], by = 0.1)[seq(2,11,by = 2)], label = FALSE)
   mtext(side = 3, "Alt", line = -2, cex = little)
   text(x = xlim[1], y = limpf[2] * 1.05, "Final size", cex = big, xpd = NA)
 
   errbar(x = 1:dim(orig)[1], y = orig[, 12], yplus = orig[, 14], frame.plot = FALSE,
     yminus = orig[, 13], xaxt = "n", ylim = limpf, pch = symb, xlim = xlim)
+    mtext(side = 3, "Orig", line = -2, cex = little)
+  axis(2, at = seq(limpf[1], limpf[2], by = 0.1)[seq(2,11,by = 2)], label = FALSE)
   errbar(x = 1:dim(alt)[1], y = alt[, 12], yplus = alt[, 14], frame.plot = FALSE,
     yminus = alt[, 13], xaxt = "n", ylim = limpf, pch = symb, xlim = xlim, yaxt = "n")
   axis(2, labels = FALSE)
+  axis(2, at = seq(limpf[1], limpf[2], by = 0.1)[seq(2,11,by = 2)], label = FALSE)
   mtext(side = 3, "Alt", line = -2, cex = little)
   text(x = xlim[1], y = limpf[2] * 1.05, "Lowest size", cex = big, xpd = NA)
 
   y <- as.numeric(orig[, 24])
   plot(x = 1:dim(orig)[1], y = y, ylim = limaa, xaxt = "n", pch = symb,
     frame.plot = FALSE, yaxt = "n", xlim = c(0, dim(orig)[1]))
-  mtext(side = 3, "Orig", line = -2, cex = little)
   y <- as.numeric(as.character(alt[, 24]))
   points(x = 1:dim(alt)[1], y = y + limaa[2] / 2, ylim = limaa, xaxt = "n", pch = symb,
     yaxt = "n")
-  axis(2, at = seq(0, limaa[2], length.out = 5),
-    labels = c(0, limaa[2]/4, limaa[2]/2, limaa[2]/4, limaa[2]/2))
-  mtext(side = 3, outer = FALSE, "AAV", line = -0.25)
+  axis(2, at = seq(0, limaa[2], length.out = 9),
+    labels = c(0, seq(limaa[2]/8, limaa[2]/2, length.out = 4),
+      seq(limaa[2]/8, limaa[2]/2, length.out = 4)))
   mtext(side = 3, "Alt", line = -20, cex = little)
-  text(x = num/2, y = limaa[2] * 1.05, "Lowest size", cex = big, xpd = NA)
+  text(x = num/2, y = limaa[2] * 1.05, "AAV", cex = big, xpd = NA)
   abline(h = limaa[2] / 2, xpd = FALSE)
   dev.off()
 
 }
-
-###############################################################################
-###############################################################################
-#### Step
-#### a
-###############################################################################
-###############################################################################
-keep <- subset(sheet, curve %in% c("a", "all", "ab") & component == 1 & dt == 0 &
-    !depl %in% c(0.60, 0.99))
-keep <- keep[order(keep$depl), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig01F1"))
-
-keep <- subset(sheet, curve %in% c("a", "all", "ab") & component == 2 & dt == 0 &
-  !depl %in% c(0.60, 0.99))
-keep <- keep[order(keep$depl), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig01F2"))
-
-keep <- subset(sheet, curve %in% c("a", "all", "ab") & component == 1 & dt == 1 &
-    !depl %in% c(0.60, 0.99))
-keep <- keep[order(keep$depl), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig01M1"))
-
-keep <- subset(sheet, curve %in% c("a", "all", "ab") & component == 2 & dt == 1 &
-  !depl %in% c(0.60, 0.99))
-keep <- keep[order(keep$depl), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig01M2"))
-
-###############################################################################
-###############################################################################
-#### Step
-#### b
-###############################################################################
-###############################################################################
-keep <- subset(sheet, curve %in% c("b", "all", "ab") & component == 1 & dt == 0 &
-  !depl %in% c(0.3))
-keep <- keep[order(keep$depl), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig02F1"))
-
-keep <- subset(sheet, curve %in% c("b", "all", "ab") & component == 2 & dt == 0 &
-  !depl %in% c(0.3))
-keep <- keep[order(keep$depl), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig02F2"))
-
-keep <- subset(sheet, curve %in% c("b", "all", "ab") & component == 1 & dt == 1 &
-  !depl %in% c(0.3))
-keep <- keep[order(keep$depl), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig02M1"))
-
-keep <- subset(sheet, curve %in% c("b", "all", "ab") & component == 2 & dt == 1 &
-  !depl %in% c(0.3))
-keep <- keep[order(keep$depl), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig02M2"))
-
-###############################################################################
-###############################################################################
-#### Step
-#### c
-###############################################################################
-###############################################################################
-keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 0 &
-  depl %in% c(0.3))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F13"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 0 &
-  depl %in% c(0.6))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F16"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 0 &
-  depl %in% c(0.99))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F19"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 0 &
-  depl %in% c(0.3))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F23"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 0 &
-  depl %in% c(0.6))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F26"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 0 &
-  depl %in% c(0.99))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F29"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 1 &
-  depl %in% c(0.3))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M13"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 1 &
-  depl %in% c(0.6))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M16"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 1 &
-  depl %in% c(0.99))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M19"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 1 &
-  depl %in% c(0.3))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M23"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 1 &
-  depl %in% c(0.6))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M26"))
-
-keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 1 &
-  depl %in% c(0.99))
-ords <- order(keep$cerror)
-keep <- keep[c(ords[-1], ords[1]), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M29"))
-
-###############################################################################
-###############################################################################
-#### Step
-#### c
-###############################################################################
-###############################################################################
-keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 0 &
-  depl %in% c(0.3))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F13"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 0 &
-  depl %in% c(0.6))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F16"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 0 &
-  depl %in% c(0.99))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F19"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 0 &
-  depl %in% c(0.3))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F23"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 0 &
-  depl %in% c(0.6))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F26"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 0 &
-  depl %in% c(0.99))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F29"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 1 &
-  depl %in% c(0.3))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M13"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 1 &
-  depl %in% c(0.6))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M16"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 1 &
-  depl %in% c(0.99))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M19"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 1 &
-  depl %in% c(0.3))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M23"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 1 &
-  depl %in% c(0.6))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M26"))
-
-keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 1 &
-  depl %in% c(0.99))
-keep <- keep[order(keep$cerror), ]
-plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M29"))
--- scripts/SC66aRMP3_Results.R
+++ scripts/SC66aRMP3_Results.R
@@ -51,6 +51,7 @@ library(xtable)
 
 dir.wk <- file.path(dir.home, "ms")
 setwd(dir.wk)
+source(file.path("..", "R", "plot_curve.R"))
 
 ###############################################################################
 ###############################################################################
@@ -177,67 +178,203 @@ system(paste("pandoc -f latex -t docx", paste0(paper, "_", mylabel, ".tex"),
 ###############################################################################
 ###############################################################################
 #### Step
-#### Response curve
+#### Response curves
 ###############################################################################
 ###############################################################################
-limtc <- c(0, 2.5)
-limpf <- c(0, 1)
-limaa <- c(0, 0.4)
 
-sheet <- read.csv(fn.basetrials, header = TRUE)
+###############################################################################
+###############################################################################
+#### Step
+#### a
+###############################################################################
+###############################################################################
 keep <- subset(sheet, curve %in% c("a", "all", "ab") & component == 1 & dt == 0 &
-  msyr == 0.01 & !depl %in% c(0.60, 0.99))
+    !depl %in% c(0.60, 0.99))
 keep <- keep[order(keep$depl), ]
-orig <- data.frame(aep100[match(keep$name, aep100$trial), ],
-  stringsAsFactors = FALSE)
-orig$AAV <- gsub("[[:punct:]]", "", orig$AAV)
-alt <- data.frame(ps4100[match(keep$name, ps4100$trial), ],
-  stringsAsFactors = FALSE)
-alt$AAV <- gsub("[[:punct:]]", "", alt$AAV)
-symb <- LETTERS[1:dim(orig)[1]]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig01F1"))
 
+keep <- subset(sheet, curve %in% c("a", "all", "ab") & component == 2 & dt == 0 &
+  !depl %in% c(0.60, 0.99))
+keep <- keep[order(keep$depl), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig01F2"))
 
-# response curve a
-jpeg(paste0(paper, "_Fig01.jpeg"), quality = 100)
-par(mfrow = c(1, 7), las = 1, mar = rep(0.1, 4), oma = c(0.2, 3, 0.2, 0.2),
-  tck = 0.05, mgp = c(3, 0.1, 0))
-errbar(x = 1:dim(orig)[1], y = orig[, 2], yplus = orig[, 4], frame.plot = FALSE,
-  yminus = orig[, 3], xaxt = "n", ylim = limtc, pch = symb, xlim = xlim)
-mtext(side = 3, outer = FALSE, "Total catch", line = -1, adj = 1)
-legend(x = -2, y = limtc[2], bty = "n", "Orig")
-errbar(x = 1:dim(alt)[1], y = alt[, 2], yplus = alt[, 4], frame.plot = FALSE,
-  yminus = alt[, 3], xaxt = "n", ylim = limtc, pch = symb, xlim = xlim, yaxt = "n")
-axis(2, labels = FALSE)
-legend(x = -2, y = limtc[2], bty = "n", "Alt")
+keep <- subset(sheet, curve %in% c("a", "all", "ab") & component == 1 & dt == 1 &
+    !depl %in% c(0.60, 0.99))
+keep <- keep[order(keep$depl), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig01M1"))
 
-errbar(x = 1:dim(orig)[1], y = orig[, 6], yplus = orig[, 8], frame.plot = FALSE,
-  yminus = orig[, 7], xaxt = "n", ylim = limpf, pch = symb, xlim = xlim)
-mtext(side = 3, outer = FALSE, "Final size", line = -1, adj = 1)
-legend(x = -2, y = limpf[2], bty = "n", "Orig")
-errbar(x = 1:dim(alt)[1], y = alt[, 6], yplus = alt[, 8], frame.plot = FALSE,
-  yminus = alt[, 7], xaxt = "n", ylim = limpf, pch = symb, xlim = xlim, yaxt = "n")
-axis(2, labels = FALSE)
-legend(x = -2, y = limpf[2], bty = "n", "Alt")
+keep <- subset(sheet, curve %in% c("a", "all", "ab") & component == 2 & dt == 1 &
+  !depl %in% c(0.60, 0.99))
+keep <- keep[order(keep$depl), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig01M2"))
 
-errbar(x = 1:dim(orig)[1], y = orig[, 12], yplus = orig[, 14], frame.plot = FALSE,
-  yminus = orig[, 13], xaxt = "n", ylim = limpf, pch = symb, xlim = xlim)
-mtext(side = 3, outer = FALSE, "Lowest size", line = -1, adj = 1)
-legend(x = -2, y = limpf[2], bty = "n", "Orig")
-errbar(x = 1:dim(alt)[1], y = alt[, 12], yplus = alt[, 14], frame.plot = FALSE,
-  yminus = alt[, 13], xaxt = "n", ylim = limpf, pch = symb, xlim = xlim, yaxt = "n")
-axis(2, labels = FALSE)
-legend(x = -2, y = limpf[2], bty = "n", "Alt")
+###############################################################################
+###############################################################################
+#### Step
+#### b
+###############################################################################
+###############################################################################
+keep <- subset(sheet, curve %in% c("b", "all", "ab") & component == 1 & dt == 0 &
+  !depl %in% c(0.3))
+keep <- keep[order(keep$depl), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig02F1"))
 
-y <- as.numeric(orig[, 24])
-plot(x = 1:dim(orig)[1], y = y, ylim = limaa, xaxt = "n", pch = symb,
-  frame.plot = FALSE, yaxt = "n")
-legend(x = 0, y = limaa[2] / 2, bty = "n", "Orig")
-y <- as.numeric(as.character(alt[, 24]))
-points(x = 1:dim(alt)[1], y = y + 0.2, ylim = limaa, xaxt = "n", pch = symb,
-  yaxt = "n")
-axis(2, at = seq(0, limaa[2], length.out = 5),
-  labels = c(0, limaa[2]/4, limaa[2]/2, limaa[2]/4, limaa[2]/2))
-mtext(side = 3, outer = FALSE, "AAV", line = -1)
-legend(x = 0, y = limaa[2], bty = "n", "Alt")
-dev.off()
+keep <- subset(sheet, curve %in% c("b", "all", "ab") & component == 2 & dt == 0 &
+  !depl %in% c(0.3))
+keep <- keep[order(keep$depl), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig02F2"))
+
+keep <- subset(sheet, curve %in% c("b", "all", "ab") & component == 1 & dt == 1 &
+  !depl %in% c(0.3))
+keep <- keep[order(keep$depl), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig02M1"))
+
+keep <- subset(sheet, curve %in% c("b", "all", "ab") & component == 2 & dt == 1 &
+  !depl %in% c(0.3))
+keep <- keep[order(keep$depl), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig02M2"))
+
+###############################################################################
+###############################################################################
+#### Step
+#### c
+###############################################################################
+###############################################################################
+keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 0 &
+  depl %in% c(0.3))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F13"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 0 &
+  depl %in% c(0.6))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F16"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 0 &
+  depl %in% c(0.99))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F19"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 0 &
+  depl %in% c(0.3))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F23"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 0 &
+  depl %in% c(0.6))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F26"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 0 &
+  depl %in% c(0.99))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03F29"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 1 &
+  depl %in% c(0.3))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M13"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 1 &
+  depl %in% c(0.6))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M16"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 1 & dt == 1 &
+  depl %in% c(0.99))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M19"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 1 &
+  depl %in% c(0.3))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M23"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 1 &
+  depl %in% c(0.6))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M26"))
+
+keep <- subset(sheet, curve %in% c("c", "all") & component == 2 & dt == 1 &
+  depl %in% c(0.99))
+ords <- order(keep$cerror)
+keep <- keep[c(ords[-1], ords[1]), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig03M29"))
+
+###############################################################################
+###############################################################################
+#### Step
+#### d
+###############################################################################
+###############################################################################
+keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 0 &
+  depl %in% c(0.3))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F13"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 0 &
+  depl %in% c(0.6))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F16"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 0 &
+  depl %in% c(0.99))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F19"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 0 &
+  depl %in% c(0.3))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F23"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 0 &
+  depl %in% c(0.6))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F26"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 0 &
+  depl %in% c(0.99))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04F29"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 1 &
+  depl %in% c(0.3))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M13"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 1 &
+  depl %in% c(0.6))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M16"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 1 & dt == 1 &
+  depl %in% c(0.99))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M19"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 1 &
+  depl %in% c(0.3))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M23"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 1 &
+  depl %in% c(0.6))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M26"))
+
+keep <- subset(sheet, curve %in% c("d", "all") & component == 2 & dt == 1 &
+  depl %in% c(0.99))
+keep <- keep[order(keep$survbias), ]
+plot_curve(aep100, ps4100, keep, paste0(paper, "_Fig04M29"))
 
