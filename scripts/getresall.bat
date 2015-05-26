ccb3b5311061203319243f0b9922e663fea5a364
kellijohnson <kellifayejohnson@gmail.com>
2015-05-26 12:51:29 -0700

    Fix bug to get all folders

--- scripts/getresall.bat
+++ scripts/getresall.bat
@@ -5,10 +5,12 @@ for /f %%h in ('dir *0 /b /ad') do (
   del RESOUT.RRR
   for /f %%f in ('dir /b /ad') do (
     cd %%f
+    echo off
     copy c:\iwccla\lib\res.exe res.exe /Y
     res.exe
     cd ..
   )
+  cd ..
 )
 cd c:\iwccla
 scripts\res2dropbox.bat
