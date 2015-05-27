c0ab94d14dedba5c051db5cb676418142367b58b
kellijohnson <kellifayejohnson@gmail.com>
2015-05-27 07:32:11 -0700

    fix columns

--- R/readRRR2.R
+++ R/readRRR2.R
@@ -1,9 +1,9 @@
 readRRR2 <- function (filename) {
-  widths <- c(14, 6, 8, 6, 6, 6, 8, 6, 6, 8, 6, 6, 8, 6, 6, 10, 8, 6, 10, 6, 11)
+  widths <- c(14, 6, 8, 6, 6, 6, 8, 6, 6, 8, 6, 6, 8, 6, 6, 9, 8)
   output <- read.fwf(filename, widths, strip.white = TRUE, stringsAsFactors = FALSE)
   colnames <- c("trial", "depl", "msyr", "tc5", "tcMed", "tc95", "tcMean",
   "pf5", "pfMed", "pf95", "pl5", "pl10", "pl25",
-  "ccc1", "ccc2", "ccc3", "AAV", "crpl1", "crpl2", "crr1", "crr2")
+  "ccc5", "cccMed", "ccc95", "AAV")
   colnames(output) <- colnames
   if (is.factor(output$AAV)) {
     output$AAV <-  gsub("[[:punct:]]$", "", output$AAV)
