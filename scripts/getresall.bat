a4deb568f732bf0beaf16c7e6e43d0224a4543f2
kellijohnson <kellifayejohnson@gmail.com>
2015-05-26 17:40:49 -0700

    Add all .RRR files for delete and remove echo, make sure that res is deleted so the new one is used

--- scripts/getresall.bat
+++ scripts/getresall.bat
@@ -1,11 +1,10 @@
 set old=%cd%
 for /f %%h in ('dir *0 /b /ad') do (
   cd %%h
-  del MSYL.RRR
-  del RESOUT.RRR
+  del *.RRR
   for /f %%f in ('dir /b /ad') do (
     cd %%f
-    echo off
+    del res.exe
     copy c:\iwccla\lib\res.exe res.exe /Y
     res.exe
     cd ..
