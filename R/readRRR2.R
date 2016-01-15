6b4d6a5f963b493b15d7f6d34a640af48cffa243
kellijohnson <kellifayejohnson@gmail.com>
2016-01-14 23:08:09 -0800

    Add readRRR2.R changes

--- R/readRRR2.R
+++ R/readRRR2.R
@@ -1,5 +1,5 @@
 readRRR2 <- function (filename) {
-  widths <- c(14, 6, 8, 6, 6, 6, 8, 6, 6, 8, 6, 6, 8, 6, 6, 9, 8)
+  widths <- c(14, 6, 7, 6, 6, 6, 9, 6, 6, 8, 6, 6, 8, 6, 6, 9, 8)
   output <- read.fwf(filename, widths, strip.white = TRUE, stringsAsFactors = FALSE)
   colnames <- c("trial", "depl", "msyr", "tc5", "tcMed", "tc95", "tcMean",
   "pf5", "pfMed", "pf95", "pl5", "pl10", "pl25",
